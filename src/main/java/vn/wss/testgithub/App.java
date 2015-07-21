a9e42b8aa960ec7c1762ce4b977393d10b8a7ce2
Ha Ninh <haninh.fami@gmail.com>
2015-07-21 09:37:04 +0700

    this is secondary commit by HN

---
+++ src/main/java/vn/wss/testgithub/App.java
@@ -0,0 +1,13 @@
+package vn.wss.testgithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
